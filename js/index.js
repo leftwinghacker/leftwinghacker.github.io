7e0cab97be101603b023cffd23079dbf03e4ef2e
Left Wing Hacker <leftwinghacker@gmail.com>
2016-08-20 21:42:41 +0200

    Add preview on ref hover

--- js/index.js
+++ js/index.js
@@ -7,4 +7,23 @@ $(document).ready(function() {
         $("#toc").removeClass("hidden");
         $("#content").removeClass("full");
     });
+    $("sup.ref").hover(function(e) {
+        if(e.type=="mouseenter") {
+            var refid=$("a",$(this)).attr("href").split("#")[1];
+            if($("div.ref-preview-"+refid).length>0) {
+                return;
+            }
+            $("div.ref-preview").each(function() {
+                var el=$(this);
+                el.fadeOut(function(a,b,c) { el.remove(); });
+            });
+            var target=$("#"+refid);
+            var x=e.pageX-$("#content").offset().left;
+            var y=$("#content").scrollTop()+e.pageY-$("#content").offset().top;
+            var preview=$("<div>").appendTo($("#content")).css({top:y,left:x}).addClass("ref-preview ref-preview-"+refid).append(target.clone());
+            setTimeout(function() {
+                preview.fadeOut(function() { preview.remove(); });
+            },5000);
+        }
+    });
 });
